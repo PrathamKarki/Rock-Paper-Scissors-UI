9d12140f8533e36f691680e85f1220b39b7c98f6
pratham karki <prathamkarki21@gmail.com>
2024-11-11 20:04:52 +0545

    Fix functionality for reset and stop play after 5 round

--- script.js
+++ script.js
@@ -1,101 +1,107 @@
+let userScore = 0;
+let computerScore = 0;
+let currentRound = 0;
+let humanChoice = '';
 const rockEl = document.querySelector('.rock');
 const paperEl = document.querySelector('.paper');
-const scissorEl = document.querySelector('.scissors');
-const humanSelectionEl = document.querySelector('#user-selection');
-const computerSelectionEl =document.querySelector('#computer-selection');
+const scissorsEl = document.querySelector('.scissors');
+const userSelectionEl = document.querySelector('#user-selection');
+const computerSelectionEl = document.querySelector('#computer-selection');
 const outcome = document.querySelector('.outcome');
+const choice = ['Rock','Paper','Scissors'];
 const resultEl = document.querySelector('.result');
-const restart = document.querySelector('.reset');
-//setting the initial score for the user and computer
-let humanScore = 0;
-let computerScore = 0;
-let round = 0;
-let humanChoice = '';
-const choice = ["Rock","Paper","Scissors"];
+const restart = document.querySelector('.reset-btn');
+const scoreComputerEl = document.querySelector('.score-computer');
+const scoreUserEl = document.querySelector('.score-user');
+const roundEl = document.querySelector('.round-count')
+let isPlaying = true;  // flag to stop game after 5 rounds
 
-
-
-rockEl.addEventListener('click',()=>{
+rockEl.addEventListener('click',function(){
+    if(isPlaying){
     humanChoice = 'Rock';
-    humanSelectionEl.textContent = humanChoice;
-    playRound();
+    userSelectionEl.textContent = humanChoice;
+    playRound(humanChoice);
+    }
 });
 
-
-paperEl.addEventListener('click',()=>{
+paperEl.addEventListener('click',function(){
+    if(isPlaying){
     humanChoice = 'Paper';
-    humanSelectionEl.textContent = humanChoice;
-    playRound();
+    userSelectionEl.textContent = humanChoice;
+    playRound(humanChoice);
+    }
+
 });
 
-scissorEl.addEventListener('click',()=>{
+scissorsEl.addEventListener('click', function(){
+    if(isPlaying){
     humanChoice = 'Scissors';
-    humanSelectionEl.textContent = humanChoice;
-    playRound();
-    
-});
+    userSelectionEl.textContent = humanChoice;
+    playRound(humanChoice);
+    }
+   
+})
 
-function generateComputerChoice(){
-    const computerChoice = choice[Math.floor(Math.random() *3)];
-    computerSelectionEl.textContent = computerChoice;
-    return computerChoice;
-}
-
-function roundOutcome(humanChoice, computerChoice){
- if(humanChoice === computerChoice){
-    outcome.textContent = "It's a Tie for this Round!"
- } else if(humanChoice === 'Rock' && computerChoice === 'Scissors' ||
- humanChoice === 'Paper' && computerChoice === 'Rock' ||
- humanChoice === 'Scissors' && computerChoice === 'Paper'){
-    outcome.textContent = "User Wins this Round!";
-    humanScore++;
-    document.querySelector('.score-user').textContent = humanScore;
- } else{
-    outcome.textContent = "Computer Wins this round";
-    computerScore++;
-    document.querySelector('.score-computer').textContent = computerScore;
- }
-    round++;
-    if(round >=5){
-        showFinalWinner();
-        playing = false;
+function computerChoice(){
+    if(isPlaying){
+    const comChoice =  choice[Math.floor(Math.random() * 3)];
+    computerSelectionEl.textContent = comChoice;
+    return comChoice;
     }
 }
 
-function playRound(){
-    if(humanChoice === '')
-        return;
-    
-    const computerChoice = generateComputerChoice();
-    roundOutcome(humanChoice,computerChoice);
-}
 
-function showFinalWinner(){
-    if(humanScore > computerScore){
-        resultEl.textContent = 'You win!';
-        resultEl.style.backgroundColor = "green";
-    } else if(humanScore < computerScore){
-        resultEl.textContent = 'You lose!';
-        resultEl.style.backgroundColor = ' #9B111E';
-    } else{
-        resultEl.textContent = 'Tie No winners!';
-        resultEl.style.backgroundColor = ' #808080';
+
+
+
+function playRound(humanChoice){
+        const computerChoiceOutcome = computerChoice();
+    if(humanChoice === computerChoiceOutcome){
+        outcome.textContent = `It's a tie for this Round!`
+    } else if(humanChoice === 'Rock' && computerChoiceOutcome === 'Scissors' ||
+        humanChoice === 'Paper' && computerChoiceOutcome === 'Rock' || humanChoice === 'Scissors' && computerChoiceOutcome ==='Paper'){
+            outcome.textContent = `Human Wins this Round!`
+            userScore++;
+            scoreUserEl.textContent = userScore;
+
+        }  else{
+            outcome.textContent = `Computer Wins this Round!`
+            computerScore++;
+            scoreComputerEl.textContent = computerScore;
+
+        }
+    currentRound++;
+    roundEl.textContent = currentRound;
+
+    if(currentRound === 5){
+        isPlaying = false;
+        if(userScore > computerScore){
+            resultEl.textContent = `Human wins the game in overall.`
+            resultEl.style.backgroundColor = '#28a745';
+        } else if(computerScore > userScore){
+            resultEl.textContent = `Computer wins the game in overall.`
+            resultEl.style.backgroundColor = '#800000'
+        } else if(userScore === computerScore){
+            resultEl.textContent = `It a tie for overall game!`
+            resultEl.style.backgroundColor = '#777';
+        }
+
     }
 
 }
 
-restart.addEventListener('click',function(){
-    humanScore = 0;
+restart.addEventListener('click', function(){
+    isPlaying = true;
+    userScore = 0;
     computerScore = 0;
-    round = 0;
-
-    humanSelectionEl.textContent = "none";
-    computerSelectionEl.textContent = "none";
-    outcome.textContent = "";
-    // resultEl.style.display = 'none';
-    resetButton.style.display = 'none';
-
-    scoreHumanEl.textContent = '0';
-    scoreComputerEl.textContent = '0';
-    resultEl.backgroundColor = '';
+    currentRound = 0;
+    humanChoice = '';
+     resultEl.textContent = `Winner is...`
+     resultEl.style.backgroundColor = '';
+     userSelectionEl.textContent = humanChoice;
+     computerSelectionEl.textContent = ''; 
+     outcome.textContent = '';
+     roundEl.textContent = currentRound; 
+     scoreUserEl.textContent = userScore; 
+     scoreUserEl.textContent = userScore; 
 })
